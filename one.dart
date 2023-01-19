fe15440839acc9ab5f8e80cd65a00dd6817bd37d
Amartya sen <amartyasen895@gmail.com>
2023-01-19 10:15:39 +0530

    updated dart

--- one.dart
+++ one.dart
@@ -1,4 +1,11 @@
-
 //my first change
 
 //my second changes
+
+void main() {
+  Map<String, String> myStringMap = {"1": "Shreyash", "2": "Hari", "3": "Ravi"};
+
+  myStringMap.forEach((key, value) {
+    print("key : ${key} - value : ${value}");
+  });
+}
